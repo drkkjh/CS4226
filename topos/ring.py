05734139e2c5312a162f87bcb966ca9841a7bc6b
Your Name <you@example.com>
2023-10-28 11:25:54 -0700

    Initialize repo

---
+++ topos/ring.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import os, sys
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info, debug
+from mininet.node import Host, RemoteController
+
+class TreeTopo( Topo ):
+    "Tree topology"
+
+    def retrieveFileContents(self, f):
+	# Get file contents out from file
+	contents = f.read().split()
+	numHosts = contents[0]
+	numSwitches = contents[1]
+	numLinks = contents[2]
+	linkDetails = contents[3:] 
+	return numHosts, numSwitches, numLinks, linkDetails	
+
+    def build( self ):
+        # Read ring.in
+        # Load configuration of Hosts, Switches, and Links
+        # You can write other functions as you need.
+	filename = 'ring.in'
+	f = open(filename, "r")
+	numHosts, numSwitches, numLinks, linkDetails = self.retrieveFileContents(f)
+	print("Number of hosts:", numHosts)
+	print("Number of switches:", numSwitches)
+	print("Number of links:", numLinks)
+	
+        # Add hosts
+        # > self.addHost('h%d' % [HOST NUMBER])
+	for i in range(1, int(numHosts) + 1):
+		self.addHost('h%d' %i)
+
+        # Add switches
+        # > sconfig = {'dpid': "%016x" % [SWITCH NUMBER]}
+        # > self.addSwitch('s%d' % [SWITCH NUMBER], **sconfig)
+	for j in range(1, int(numSwitches) + 1):
+		sconfig = {'dpid': "%016x" % j}
+		self.addSwitch('s%d' % i, **sconfig)
+
+        # Add links
+        # > self.addLink([HOST1], [HOST2])
+	for k in range(int(numLinks)):
+		linkDetail = linkDetails[k].split(',')
+		host1 = linkDetail[0]
+		host2 = linkDetail[1]
+		self.addLink(host1, host2)
+        
+                    
+topos = { 'sdnip' : ( lambda: TreeTopo() ) }
+
+if __name__ == '__main__':
+    sys.path.insert(1, '/home/sdn/onos/topos')
+    from onosnet import run
+    run( TreeTopo() )
